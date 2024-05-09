cb4a03f73975cf293b177e4605b2a733de898f8f
Pranavvishwakarma <pranav213234@gmail.com>
2024-05-09 15:59:55 +0530

    changes

--- src/components/Transformation.jsx
+++ src/components/Transformation.jsx
@@ -9,19 +9,19 @@ const Transformation = ({ testimonialref }) => {
       id: 1,
       story:
         ' "Their approach to wellness is truly holistic, taking into account all aspects of my life including nutrition, exercise, stress management, and self-care. My mentor was incredibly knowledgeable and supportive, providing me with personalized guidance and practical tools to help me achieve my goals. With their help, I was able to make significant improvements in my overall well-being and feel more empowered to take charge of my health. I highly recommend this company to anyone looking for a comprehensive and transformative wellness experience." ',
-      name: "RAJ SONI ",
+     
     },
     {
       id: 2,
       story:
         ' "I am amazed at the progress I have made. Their team of experts has helped me identify and overcome obstacles that were holding me back from achieving my full potential. Their approach to wellness is unique, combining evidence-based practices with a compassionate and supportive approach. They have helped me develop new habits and strategies for success that have improved my overall well-being and productivity. I am truly grateful for their support and highly recommend their services to anyone who is looking to live their best life." ',
-      name: "RUDRA SINGH",
+      
     },
     {
       id: 3,
       story:
         '"Their team is incredibly skilled, empathetic, and supportive, and they have helped me make significant improvements in my life. With their guidance, I have been able to overcome my limiting beliefs and tap into my full potential. Their approach to wellness is truly transformative, focusing on creating sustainable habits that promote overall well-being. I am extremely  grateful for their continued support, I highly recommend their services to anyone who is looking to live their best life." ',
-      name: "VISHAL VERMA",
+    
     },
   ];
 
